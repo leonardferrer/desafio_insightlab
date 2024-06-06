7c522e7c6ce57aa32de2072af361b474390d98e8
LeonardFerrer <leonnardo.fer@gmail.com>
2024-06-06 04:39:33 -0300

    create project

---
+++ src/main/java/com/example/desafio_insightlab/DesafioInsightlabApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio_insightlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioInsightlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioInsightlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafio_insightlab/DesafioInsightlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_insightlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioInsightlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
